ca4526439b0772c1c5335981e4c5cd527ca7d8fc
Ben Rucker <brucker117@gmail.com>
2021-03-12 00:06:20 -0500

    primitive game loop in main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.io.File;
+import java.nio.file.Files;
+
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,6 +22,26 @@ public class Main extends Application {
 
         Canvas canvas = new Canvas(1225, 600);
         root.getChildren().add(canvas);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Entity test = new Entity(canvas.getGraphicsContext2D());
+
+        // Image scrum = new Image("https://cdn.discordapp.com/attachments/801173300415037504/815023513903169546/S.C.R.U.M..gif");        
+
+        final long startNanoTime = System.nanoTime();
+ 
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                // background image clears canvas
+                gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+                test.update();
+                test.draw();
+            }
+        }.start();
+
 
         stage.show();
     }
