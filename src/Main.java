6cee7cb90b391e0a9b0f12b4ea74644c24cb999b
Ben Rucker <brucker117@gmail.com>
2021-03-12 16:02:26 -0500

    change to keyframe loop

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ import java.io.File;
 import java.nio.file.Files;
 
 import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
-import javafx.scene.Scene;
+// import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
 
@@ -29,19 +34,34 @@ public class Main extends Application {
 
         // Image scrum = new Image("https://cdn.discordapp.com/attachments/801173300415037504/815023513903169546/S.C.R.U.M..gif");        
 
-        final long startNanoTime = System.nanoTime();
- 
-        new AnimationTimer()
-        {
-            public void handle(long currentNanoTime)
+        // Set up game loop
+        Timeline gameLoop = new Timeline();
+        gameLoop.setCycleCount( Timeline.INDEFINITE );
+        
+        final long timeStart = System.currentTimeMillis();
+        
+        KeyFrame kf = new KeyFrame(
+            Duration.seconds(0.033),
+            new EventHandler<ActionEvent>()
             {
-                // background image clears canvas
-                gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
-                test.update();
-                test.draw();
-            }
-        }.start();
-
+                public void handle(ActionEvent ae)
+                {
+                    double t = (System.currentTimeMillis() - timeStart) / 1000.0; 
+                                
+                    // double x = 232 + 128 * Math.cos(t);
+                    // double y = 232 + 128 * Math.sin(t);
+                    
+                    // Clear the canvas
+                    gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+                    
+                    // background image clears canvas
+                    // gc.drawImage(scrum, 0, 0 );
+                    currentScene.draw();
+                }
+            });
+        
+        gameLoop.getKeyFrames().add( kf );
+        gameLoop.play();
 
         stage.show();
     }
