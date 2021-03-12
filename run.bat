575172208f6abb5958dffe713ca0d1561051b7cd
Ben Rucker <brucker117@gmail.com>
2021-03-11 22:50:09 -0500

    add run.bat

---
+++ run.bat
@@ -0,0 +1,5 @@
+cd src
+set PATH_TO_FX="..\javafx-sdk-11.0.2\lib"
+javac --module-path %PATH_TO_FX% --add-modules javafx.controls Main.java
+java --module-path %PATH_TO_FX% --add-modules javafx.controls Main
+cd ..
