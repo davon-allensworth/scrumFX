10d904316a6ac90c0d9990706f1f919ccbac2090
Ben Rucker <brucker117@gmail.com>
2021-04-19 18:42:07 -0400

    remove TODO

--- src/Scene.java
+++ src/Scene.java
@@ -6,7 +6,6 @@ public abstract class Scene extends javafx.scene.Scene {
     
     public Scene(Parent arg0) {
         super(arg0);
-        //TODO Auto-generated constructor stub
     }
 
     List<Entity> entities = null;
